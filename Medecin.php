dfa0fc457453f8dc09cf787501d4978bc278c0c1
chihiroprog <stutuart.barthe@gmail.com>
2023-11-24 12:00:58 +0100

    reset

--- Medecin.php
+++ Medecin.php
@@ -1,13 +1,10 @@
 <?php
+include 'Rendez_vous.php';
 class Medecin extends User{
 }
+
 class Secretaire{
-    public function cree_rendez_vous(){
-        $rendez_vous = new Rendez_vous();
-        $rendez_vous->date_rendez_vous_minute = new DateTime("2020-01-01 00:00:00");
-        $rendez_vous->durée = 30;
-        $rendez_vous->user = new Usager();
-        return $rendez_vous;
+
+
     }
-}
-?>
+ ?>
--- Rendez_vous.php
+++ Rendez_vous.php
@@ -1,8 +1,17 @@
 <?php
 class Rendez_vous{
     private dateTime $date_rendez_vous_minute;
-    private dateTime $durée;
-    private User $usager;
+    private dateTime $duree;
+    private Usager $usager;
     private Medecin $medecin;
+
+    public function __construct($date_rendez_vous_minute, $duree, $usager, $medecin){
+        
+        $this->date_rendez_vous_minute = $date_rendez_vous_minute;
+        $this->duree = $duree;
+        $this->usager = $usager;
+        $this->medecin = $medecin;
+    }
+
 }
 ?>
