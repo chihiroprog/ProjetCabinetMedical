2681cd68716278645d51eee403534c9257883c76
Enzo <enzo.delacour@etu.iut-tlse3.fr>
2023-11-29 22:51:22 +0100

    ajout_user

---
+++ php/ajout_usagers.php
@@ -0,0 +1,39 @@
+<?php
+require '../php/db-config.php';
+try {
+    $options = [
+        PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ];
+
+    $linkpdo = new PDO($DB_DSN, $DB_USER, $DB_PASS, $options);
+
+
+
+} catch (Exception $pe) {
+    echo 'ERREUR : ' . $pe->getMessage();
+}
+
+    $civilite = $_POST['civilite'];
+    $nom = $_POST['nom'];
+    $prenom = $_POST['prenom'];
+    $adresse = $_POST['adresse'];
+    $date_naissance = $_POST['date_naissance'];
+    $lieu_naissance = $_POST['lieu_naissance'];
+    $numero_securite_social = $_POST['numero_securite_social'];
+
+    $req = $linkpdo->prepare('INSERT INTO usager (civilite ,nom, prenom, adresse, date_naissance,lieu_naissance, numero_securite_social ) 
+    VALUES (:civilite,:nom, :prenom, :adresse, :date_naissance,:lieu_naissance, :numero_securite_social )');
+    
+    $req->execute(array(
+        'nom' => $nom,
+        'civilite' => $civilite,
+        'prenom' => $prenom,
+        'adresse' => $adresse,
+        'date_naissance' => $date_naissance,
+        'lieu_naissance' => $lieu_naissance,
+        'numero_securite_social' => $numero_securite_social,
+
+    ));
+    exit();
+?>
