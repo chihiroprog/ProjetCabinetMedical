8d714e17126545ba053083bf36890473ec9cdb61
EnzoTlz <enzo31del@gmail.com>
2024-01-09 12:48:36 +0100

    add table secretaire + login with mdp & first name (forget git add )

---
+++ back_end/Login/loginSecretaire.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once '../Objects/dbConfig.php';
+require_once '../Objects/Secretaire.php';
+require_once 'login.php';
+
+    checkInputToLoginSecretaire($_POST);
+
+    $commandToLoginSecretaire = setCommandToLoginSecretaire($_POST);
+    var_dump($commandToLoginSecretaire);
+    $commandToLoginSecretaire->CheckSecretaireExist();
+
+    function checkInputToLoginSecretaire($POST){
+
+        if(!isset($POST['mdp'])){
+            exceptions_error_handler('mdp pas fait');
+        }
+
+        if(!isset($POST['prenom'])){
+            exceptions_error_handler('prenom pas fait');
+        }
+    }
+    function exceptions_error_handler($message) {
+        throw new ErrorException($message);
+    }
+    function setCommandToLoginSecretaire($POST){
+        $commandToLoginSecretaireToReturn = new Secretaire();
+        $commandToLoginSecretaireToReturn->setPrenom($POST['prenom']);
+        $commandToLoginSecretaireToReturn->setMdp($POST['mdp']);
+
+        return $commandToLoginSecretaireToReturn;
+    }
+?>
+
+
+
+
+
---
+++ back_end/Objects/Secretaire.php
@@ -0,0 +1,48 @@
+<?php
+require_once 'dbConfig.php';
+class Secretaire{
+    private DbConfig $dbConfig;
+    private $nom;
+    private $prenom;
+    private $mdp;
+    private $id;
+
+    public function __construct(){
+        $this->dbConfig = DbConfig::getDbConfig();
+    
+}
+
+    public function CheckSecretaireExist(){
+        try {
+            $req = $this->dbConfig->getPDO()->prepare('SELECT * FROM Secretaire WHERE mots_de_passe = :mdp AND prenom = :prenom');
+
+            $req->execute(array(
+                'prenom' => $this->prenom,
+                'mdp' => $this->mdp,
+            ));
+            $secretaire = $req->fetch();
+            if ($secretaire) {
+                header('Location: ../../front_end/Consultations.php');
+                exit;
+            } else {
+                header('Location: ../../front_end/index.html');
+                exit;
+            }
+    
+        } catch (Exception $pe) {echo 'ERREUR : ' . $pe->getMessage();}
+    }
+
+    public function setNom($nom){
+        $this->nom = $nom;
+    }
+    public function setPrenom($prenom){
+        $this->prenom = $prenom;
+    }
+    public function setMdp($mdp){
+        $this->mdp = $mdp;
+    }
+    public function setId($id){
+        $this->id = $id;
+    }
+}
+?>
